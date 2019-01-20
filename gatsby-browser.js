273c885b6cc6792778ea9b7e859c183a04cde050
darragh oriordan <darragh.oriordan@gmail.com>
2019-01-21 08:46:32 +1300

    add reload prompt

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,14 @@
 import 'typeface-lato'
 import 'prismjs/themes/prism-tomorrow.css'
 import './src/styles/global.scss'
+
+exports.onServiceWorkerUpdateFound = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
