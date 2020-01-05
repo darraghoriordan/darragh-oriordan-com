5156f01ec0724eca59720cb60222301be700bc48
darragh oriordan <darragh.oriordan@gmail.com>
2020-01-05 20:16:41 +1300

    add form with formik

--- src/components/EmailListForm.tsx
+++ src/components/EmailListForm.tsx
@@ -1,41 +1,92 @@
+import { Form, Formik, useField } from 'formik'
 import addToMailchimp from 'gatsby-plugin-mailchimp'
-import React, { useState } from 'react'
+import React, { SFC } from 'react'
+import * as Yup from 'yup'
 
-const EmailListForm: React.FunctionComponent<{}> = () => {
-  const [email, setEmail] = useState('')
-  const [subscribed, setSubscribed] = useState(false)
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-
-    addToMailchimp(email)
-      .then(data => {
-        setSubscribed(true)
-      })
-      .catch((error: Error) => {
-        // Errors in here are client side
-        // Mailchimp always returns a 200
-      })
-  }
-
-  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.currentTarget.value)
-  }
-
+interface IEmailFormTextInputProps {
+  label: string
+}
+const EmailFormTextInput: SFC<IEmailFormTextInputProps &
+  React.HTMLProps<HTMLInputElement>> = ({ label, ...props }) => {
+  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+  // which we can spread on <input> and alse replace ErrorMessage entirely.
+  const [field, meta] = useField(props)
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Subscribe for updates</h2>
-      <div>
-        <input
-          placeholder="Type your email address"
-          name="email"
-          type="email"
-          onChange={handleEmailChange}
-        />
-        <button type="submit">Get Updates</button>
-        {subscribed && <}
-      </div>
-    </form>
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  )
+}
+
+interface IEmailFormCheckboxProps {
+  label: string
+}
+const EmailFormCheckbox: SFC<IEmailFormCheckboxProps &
+  React.HTMLProps<HTMLInputElement>> = ({ children, ...props }) => {
+  const [field, meta] = useField({ ...props, type: 'checkbox' })
+  return (
+    <>
+      <label className="checkbox">
+        <input {...field} {...props} type="checkbox" />
+        {children}
+      </label>
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  )
+}
+
+// And now we can use these
+const EmailListForm: React.SFC<{}> = () => {
+  return (
+    <>
+      <h1>Subscribe for updates!</h1>
+      <Formik
+        initialValues={{
+          acceptedTerms: false,
+          email: '',
+        }}
+        validationSchema={Yup.object({
+          acceptedTerms: Yup.boolean()
+            .required('Required')
+            .oneOf([true], 'You must accept the terms and conditions.'),
+          email: Yup.string()
+            .email('Invalid email addresss`')
+            .required('Required'),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          addToMailchimp(values.email)
+            .then(() => {
+              // redirect to thanks
+              setSubmitting(false)
+            })
+            .catch(() => {
+              // Errors in here are client side
+              // Mailchimp always returns a 200
+            })
+        }}
+      >
+        <Form>
+          <EmailFormTextInput
+            label="Email Address"
+            name="email"
+            type="email"
+            placeholder="jane@formik.com"
+          />
+
+          <EmailFormCheckbox name="acceptedTerms" label="">
+            I accept the terms and conditions
+          </EmailFormCheckbox>
+
+          <button type="submit">Get Updates</button>
+        </Form>
+      </Formik>
+    </>
   )
 }
 
